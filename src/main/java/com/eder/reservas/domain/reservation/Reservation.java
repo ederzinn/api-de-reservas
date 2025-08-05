eeda46fad3339b4c98357fa1ae3cadf8595e56a1
ederzinn <ederdetrabalho@gmail.com>
2025-08-05 10:56:03 -0300

    feat created Entity Reservation and ReservationStatus enum

---
+++ src/main/java/com/eder/reservas/domain/reservation/Reservation.java
@@ -0,0 +1,30 @@
+package com.eder.reservas.domain.reservation;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reservations")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    private UUID user_id;
+    @ManyToOne
+    private UUID table_id;
+
+    private LocalDateTime reservationDateTime;
+    private ReservationStatus status;
+}
