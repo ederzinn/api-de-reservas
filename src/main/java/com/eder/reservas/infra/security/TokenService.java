fbf475ce841d6de2e1350954ab98c654c28b797f
ederzinn <ederdetrabalho@gmail.com>
2025-08-06 10:47:18 -0300

    feat removed unused import

--- src/main/java/com/eder/reservas/infra/security/TokenService.java
+++ src/main/java/com/eder/reservas/infra/security/TokenService.java
@@ -5,8 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.eder.reservas.domain.user.User;
-import com.eder.reservas.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
